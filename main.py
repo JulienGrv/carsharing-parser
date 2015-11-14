98f516bdcf96abd3b94048648dd727e4af394a27
Julien Grave <darkin@live.fr>
2015-11-14 19:15:54 +0000

    blablacar method

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import sys
 import codecs
 import urllib3
+import urllib.request as urllib
 import certifi
 import bs4 as BeautifulSoup
 
@@ -37,11 +38,31 @@ def save_html(html, file_name):
     file.close()
 
 ##
+# read html content in a file
+# file_name - File name
+##
+def open_html(file_name):
+    file = open(file_name + '.html', "r")
+    for line in file.readlines():
+        print(line)
+    file.close()
+
+##
+# Blablacar link
+# origin
+# destination
+# date
+##
+def blablacar_url(origin, destination, date):
+   return u"https://www.blablacar.fr/search?fn="+origin+"&tn="+destination+"&db="+date+"&sort=trip_date&order=asc&limit=1000"
+
+##
 # main
 ##
 def main():
     print("Hello World!")
-    url = u"https://www.blablacar.fr/trajets/paris/bordeaux/#?fn=Paris&fc=48.856614%7C2.352222&fcc=FR&tn=Bordeaux&tc=44.837789%7C-0.57918&tcc=FR&db=20/11/2015&sort=trip_price_euro&order=asc&limit=100"
+    # url = u"https://www.blablacar.fr/trajets/paris/bordeaux/#?fn=Paris&fc=48.856614%7C2.352222&fcc=FR&tn=Bordeaux&tc=44.837789%7C-0.57918&tcc=FR&db=20/11/2015&sort=trip_price_euro&order=asc&limit=100"
+    url = blablacar_url("Paris", "Bordeaux", "20/11/2015")
     resp = http_request(url)
     html = html_content(resp)
     resp.close()
@@ -49,6 +70,7 @@ def main():
     soup = BeautifulSoup.BeautifulSoup(html, "lxml")
     for p in soup.find_all('h2', 'username'):
         print(p)
+    # open_html("test")
 
 main()
 os.system("PAUSE")
